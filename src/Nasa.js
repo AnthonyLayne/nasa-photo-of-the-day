a965d23b63c64dec525016686490c18bff149667
Anthony Layne <kepheus.ds@gmail.com>
2022-02-16 17:53:00 -0500

    put title, explaination and image

---
+++ src/Nasa.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Nasa = (props) => {
+  return (
+    <div className="nasa-image-project">
+      <h3>{props.photo.title}</h3>
+      <p>{props.photo.date}</p>
+      <img src={props.photo.hdurl} />
+      <p>{props.photo.explanation}</p>
+    </div>
+  );
+};
+
+export default Nasa;
